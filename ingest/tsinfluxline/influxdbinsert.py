ebcfd205404b43aaf47f69a71d8a55d790692427
Pedro Ferreira <pedrotadim@outlook.com>
2017-06-02 15:58:46 +0200

    Fixed more corner cases in the InfluxDB discovery. Also the columns weren't created in proper order.

--- ingest/tsinfluxline/influxdbinsert.py
+++ ingest/tsinfluxline/influxdbinsert.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import OrderedDict
 
 from ingest.monetdb.mapiconnection import get_mapi_connection
 from ingest.monetdb.naming import DATABASE_SCHEMA, get_default_timestamp_value, INFLUXDB_TEXT_TYPE, \
@@ -12,6 +13,8 @@ def flush(discovery, found_metrics, values_to_insert):
     if discovery:
         for metric, values in found_metrics.items():
             if not values['created']:
+                tuples_array = map(lambda x: (x['name'], {'type': x['type'], 'isTag': x['isTag']}), values['columns'])
+                values['columns'] = OrderedDict(tuples_array)
                 create_stream_from_influxdb(metric, values['columns'])
                 values['created'] = True
 
@@ -46,7 +49,7 @@ def insert_influxdb_values_savage(lines: str, discovery: bool=False) -> None:
         if metric_name not in found_metrics:
             values_to_insert[metric_name] = []
             if discovery:
-                found_metrics[metric_name] = {'columns': {}, 'created': False}
+                found_metrics[metric_name] = {'columns': [], 'created': False}
             new_metric = True
         else:
             new_metric = False
@@ -61,14 +64,18 @@ def insert_influxdb_values_savage(lines: str, discovery: bool=False) -> None:
                 while lines[i] != '=':
                     i += 1
                 if new_metric:
-                    next_tag_name = lines[(j+1):i]
-                    found_metrics[metric_name]['columns'][next_tag_name] = {'type': INFLUXDB_TEXT_TYPE, 'isTag': True}
+                    next_tag_name = lines[j:i]
+                    found_metrics[metric_name]['columns'].append({'name': next_tag_name,
+                                                                  'type': INFLUXDB_TEXT_TYPE, 'isTag': True})
 
                 i += 1
                 j = i
                 while lines[i] not in (',', ' '):
                     i += 1
                 inserts.append(lines[j:i])
+
+                if lines[i] == ',':
+                    i += 1
 
         i += 1
         # parse the columns
@@ -77,7 +84,7 @@ def insert_influxdb_values_savage(lines: str, discovery: bool=False) -> None:
             while lines[i] != '=':
                 i += 1
             if new_metric and discovery:
-                next_column_name = lines[(j+1):i]
+                next_column_name = lines[j:i]
 
             i += 1
             j = i
@@ -88,23 +95,30 @@ def insert_influxdb_values_savage(lines: str, discovery: bool=False) -> None:
             if next_insert in ('t', 'T', 'true', 'True', 'TRUE'):
                 inserts.append('1')
                 if new_metric and discovery:
-                    found_metrics[metric_name]['columns'][next_column_name] = {'type': INFLUXDB_BOOL_TYPE, 'isTag': False}
+                    found_metrics[metric_name]['columns'].append({'name': next_column_name,
+                                                                  'type': INFLUXDB_BOOL_TYPE, 'isTag': False})
             if next_insert in ('f', 'F', 'false', 'False', 'FALSE'):
                 inserts.append('0')
                 if new_metric and discovery:
-                    found_metrics[metric_name]['columns'][next_column_name] = {'type': INFLUXDB_BOOL_TYPE, 'isTag': False}
+                    found_metrics[metric_name]['columns'].append({'name': next_column_name,
+                                                                  'type': INFLUXDB_BOOL_TYPE, 'isTag': False})
             elif next_insert.startswith('"'):
                 inserts.append(next_insert[1:-1].replace('\\"', '"'))
                 if new_metric and discovery:
-                    found_metrics[metric_name]['columns'][next_column_name] = {'type': INFLUXDB_TEXT_TYPE, 'isTag': False}
+                    found_metrics[metric_name]['columns'].append({'name': next_column_name,
+                                                                  'type': INFLUXDB_TEXT_TYPE, 'isTag': False})
             elif next_insert.endswith('i'):
                 inserts.append(next_insert[:-1])
                 if new_metric and discovery:
-                    found_metrics[metric_name]['columns'][next_column_name] = {'type': INFLUXDB_INTEGER_TYPE, 'isTag': False}
+                    found_metrics[metric_name]['columns'].append({'name': next_column_name,
+                                                                  'type': INFLUXDB_INTEGER_TYPE, 'isTag': False})
             else:
                 inserts.append(next_insert)
                 if new_metric and discovery:
-                    found_metrics[metric_name]['columns'][next_column_name] = {'type': INFLUXDB_FLOAT_TYPE, 'isTag': False}
+                    found_metrics[metric_name]['columns'].append({'name': next_column_name,
+                                                                  'type': INFLUXDB_FLOAT_TYPE, 'isTag': False})
+
+            i += 1
 
         # parse the timestamp
         if i < length_lines and lines[i] == ' ':
